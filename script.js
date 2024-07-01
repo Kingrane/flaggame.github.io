2d90e8d928e3168cd1f7269055f0500193997718
kingrane <152076926+Kingrane@users.noreply.github.com>
2024-07-01 20:53:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,18 +15,32 @@ const flags = [
   { name: "Польша", image: "https://flagcdn.com/w320/pl.png" },
   { name: "Аргентина", image: "https://flagcdn.com/w320/ar.png" },
   { name: "Панама", image: "https://flagcdn.com/w320/pa.png" },
-  
+  { name: "Уругвай", image: "https://flagcdn.com/w320/uy.png" },
+  { name: "Лаос", image: "https://flagcdn.com/w320/la.png" },
+  { name: "Пакистан", image: "https://flagcdn.com/w320/pk.png" },
+  { name: "ОАЭ", image: "https://flagcdn.com/w320/ae.png" },
+  { name: "Йемен", image: "https://flagcdn.com/w320/ye.png" },
+  { name: "Сирия", image: "https://flagcdn.com/w320/sy.png" },
+  { name: "Грузия", image: "https://flagcdn.com/w320/ge.png" },
+  { name: "Бельгия", image: "https://flagcdn.com/w320/be.png" },
+  { name: "Белоруссия", image: "https://flagcdn.com/w320/by.png" },
+  { name: "Мальта", image: "https://flagcdn.com/w320/mt.png" },
+  { name: "Иордания", image: "https://flagcdn.com/w320/jo.png" },
+  { name: "Колумбия", image: "https://flagcdn.com/w320/co.png" },
+  { name: "Малайзия", image: "https://flagcdn.com/w320/my.png" },
+
 ];
 
 let score = 0;
 let currentFlag;
 let previousFlag;
+let recentFlags = [];
 
 function generateOptions() {
   const options = [currentFlag];
   while (options.length < 3) {
     const randomIndex = Math.floor(Math.random() * flags.length);
-    if (!options.includes(flags[randomIndex]) && flags[randomIndex] !== previousFlag) {
+    if (!options.includes(flags[randomIndex]) && !recentFlags.includes(flags[randomIndex])) {
       options.push(flags[randomIndex]);
     }
   }
@@ -35,10 +49,19 @@ function generateOptions() {
 }
 
 function displayFlag() {
+  let newFlag;
   do {
-    currentFlag = flags[Math.floor(Math.random() * flags.length)];
-  } while (currentFlag === previousFlag);
-  previousFlag = currentFlag;
+    newFlag = flags[Math.floor(Math.random() * flags.length)];
+  } while (recentFlags.includes(newFlag));
+
+  currentFlag = newFlag;
+
+  recentFlags.unshift(currentFlag);
+
+  if (recentFlags.length > 4) {
+    recentFlags.pop();
+  }
+
   document.getElementById("flag").src = currentFlag.image;
   const options = generateOptions();
   document.getElementById("option1").textContent = options[0].name;
